9c86032ff1d9737c436e1fa0faba98871960c0e2
guilhermemm-dev <guilhermemm.dev@gmail.com>
2023-01-15 13:16:51 +0000

    css

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,11 +30,16 @@ type Image = {
 
 export default function Gallery({ images }: { images: Image[] }) {
   return (
-    <div className="max-w-2xl px-4 py-16 mx-auto sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-      <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-        {images.map((image) => (
-          <BlurImage key={image.id} image={image} />
-        ))}
+    <div className="contianer">
+      <h1 className="flex items-center justify-center w-full mx-auto mt-6 text-2xl font-bold text-gray-700">
+        My Favourites Wallpapers Gallery
+      </h1>
+      <div className="max-w-2xl px-4 py-16 mx-auto sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          {images.map((image) => (
+            <BlurImage key={image.id} image={image} />
+          ))}
+        </div>
       </div>
     </div>
   )
@@ -44,24 +49,28 @@ function BlurImage({ image }: { image: Image }) {
   const [isLoading, setLoading] = useState(true)
 
   return (
-    <a href={image.href} className="group">
-      <div className="w-full overflow-hidden bg-gray-200 rounded-lg aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8">
-        <Image
-          alt=""
-          src={image.imageSrc}
-          layout="fill"
-          objectFit="cover"
-          className={cn(
-            'duration-700 ease-in-out group-hover:opacity-75',
-            isLoading
-              ? 'scale-110 blur-2xl grayscale'
-              : 'scale-100 blur-0 grayscale-0'
-          )}
-          onLoadingComplete={() => setLoading(false)}
-        />
-      </div>
-      <h3 className="mt-4 text-sm text-gray-700">{image.name}</h3>
-      <p className="mt-1 text-lg font-medium text-gray-900">{image.username}</p>
-    </a>
+    <main>
+      <a href={image.href} className="group">
+        <div className="w-full overflow-hidden bg-gray-200 rounded-lg aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8">
+          <Image
+            alt=""
+            src={image.imageSrc}
+            layout="fill"
+            objectFit="cover"
+            className={cn(
+              'duration-700 ease-in-out group-hover:opacity-75',
+              isLoading
+                ? 'scale-110 blur-2xl grayscale'
+                : 'scale-100 blur-0 grayscale-0'
+            )}
+            onLoadingComplete={() => setLoading(false)}
+          />
+        </div>
+        <h3 className="mt-4 text-sm text-gray-700">{image.name}</h3>
+        <p className="mt-1 text-lg font-medium text-gray-900">
+          {image.username}
+        </p>
+      </a>
+    </main>
   )
 }
