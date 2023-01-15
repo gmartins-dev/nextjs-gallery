6daaf93378c7a55f10fa9a9bdfbc9f8811338e2c
guilhermemm-dev <guilhermemm.dev@gmail.com>
2023-01-15 12:21:43 +0000

    tests

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,20 @@
 import Image from 'next/image'
 import { useState } from 'react'
-import { createClient } from '@supabase/supabase-js'
+// import { createClient } from '@supabase/supabase-js'
 
-export async function getStaticProps() {
-  const supabaseAdmin = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL || '',
-    process.env.SUPABASE_SERVICE_ROLE_KEY || ''
-  )
+// export async function getStaticProps() {
+//   const supabaseAdmin = createClient(
+//     process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+//     process.env.SUPABASE_SERVICE_ROLE_KEY || ''
+//   )
 
-  const { data } = await supabaseAdmin.from('images').select('*').order('id')
-  return {
-    props: {
-      images: data,
-    },
-  }
-}
+//   const { data } = await supabaseAdmin.from('images').select('*').order('id')
+//   return {
+//     props: {
+//       images: data,
+//     },
+//   }
+// }
 
 function cn(...classes: string[]) {
   return classes.filter(Boolean).join(' ')
@@ -30,7 +30,7 @@ type Image = {
 
 export default function Gallery({ images }: { images: Image[] }) {
   return (
-    <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+    <div className="max-w-2xl px-4 py-16 mx-auto sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
       <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
         {images.map((image) => (
           <BlurImage key={image.id} image={image} />
@@ -45,7 +45,7 @@ function BlurImage({ image }: { image: Image }) {
 
   return (
     <a href={image.href} className="group">
-      <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+      <div className="w-full overflow-hidden bg-gray-200 rounded-lg aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8">
         <Image
           alt=""
           src={image.imageSrc}
